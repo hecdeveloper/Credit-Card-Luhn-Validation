4bdc1cfbb09b8d4888ea01219374f8aec472d836
Hector Ibarra <hibarra@imaginuity.com>
2023-08-11 19:22:01 -0500

    add input validation

--- src/Components/CreditCardForm.jsx
+++ src/Components/CreditCardForm.jsx
@@ -1,51 +1,59 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 const CreditCardForm = () => {
-    const [cardNumber, setCardNumber] = useState('');
-    const [isValid, setIsValid] = useState(null);
-    const [loading, setLoading] = useState(false); 
+  const [cardNumber, setCardNumber] = useState("");
+  const [isValid, setIsValid] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    const validateCard = async () => {
-        if (!cardNumber || cardNumber.trim() === "") {
-            return;
-        }
+  const validateCard = async () => {
+    if (!cardNumber || cardNumber.trim() === "") {
+      return;
+    }
 
-        setLoading(true);
+    setLoading(true);
 
-        console.log("Validating card:", cardNumber);
+    console.log("Validating card:", cardNumber);
 
-        try {
-            const response = await axios.post('https://card-validation-8778dda2604a.herokuapp.com/validate', { cardNumber });         
-            setIsValid(response.data.isValid);
-        } catch (error) {
-            console.error("Error validating card:", error);
-        } finally {
-            setLoading(false);
-        }
-    };
+    try {
+      const response = await axios.post(
+        "https://card-validation-8778dda2604a.herokuapp.com/validate",
+        { cardNumber }
+      );
+      setIsValid(response.data.isValid);
+    } catch (error) {
+      console.error("Error validating card:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return (
-        <div className={`cardform ${isValid ? 'is-valid' : ''}`}>
-            <h1>Credit Card Validation</h1>
+  return (
+    <div className={`cardform ${isValid ? "is-valid" : ""}`}>
+      <h1>Credit Card Validation</h1>
 
-            <input
-                type="text"
-                value={cardNumber}
-                onChange={(e) => setCardNumber(e.target.value)}
-                placeholder="Enter your card number"
-                className="card-input"
-            />
+      <input
+        type="text"
+        value={cardNumber}
+        onChange={(e) => {
+          setCardNumber(e.target.value);
+          if (!e.target.value || e.target.value.trim() === "") {
+            setIsValid(null);
+          }
+        }}
+        placeholder="Enter your card number"
+        className="card-input"
+      />
 
-            <button onClick={validateCard} className="validate-button">
-                {loading ? "Validating..." : "Validate"}
-            </button>
+      <button onClick={validateCard} className="validate-button">
+        {loading ? "Validating..." : "Validate"}
+      </button>
 
-            {isValid !== null && (
-                <p>{isValid ? "Valid Card Number!" : "Invalid Card Number."}</p>
-            )}
-        </div>
-    );
-}
+      {isValid !== null && (
+        <p>{isValid ? "Valid Card Number!" : "Invalid Card Number."}</p>
+      )}
+    </div>
+  );
+};
 
 export default CreditCardForm;
