e4e436a09d5204cefbad99f1a9e63c65e8ac65cf
Hector Ibarra <hibarra@imaginuity.com>
2023-08-11 22:50:14 -0500

    card styles

--- src/Components/CreditCardForm.jsx
+++ src/Components/CreditCardForm.jsx
@@ -1,50 +1,79 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 const CreditCardForm = () => {
-    const [cardNumber, setCardNumber] = useState('');
-    const [isValid, setIsValid] = useState(null);
-    const [loading, setLoading] = useState(false); 
+  const [cardNumber, setCardNumber] = useState("");
+  const [isValid, setIsValid] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    const validateCard = async () => {
-        if (!cardNumber || cardNumber.trim() === "") {
-            return;
-        }
+  const formatCardNumber = (value) => {
+    // Removing all non-digits
+    const cleaned = value.replace(/\D+/g, "");
 
-        setLoading(true);
+    // Adding spaces every 4 characters
+    const formatted = cleaned.match(/.{1,4}/g)?.join(" ") || "";
 
-        try {
-            const response = await axios.post('http://localhost:5000/validate', { cardNumber });
-             // const response = await axios.post('https://luhn-valid-8b66add23927.herokucardform.com/validate', { cardNumber }); // This is the deployed version.            
-            setIsValid(response.data.isValid);
-        } catch (error) {
-            console.error("Error validating card:", error);
-        } finally {
-            setLoading(false);
-        }
-    };
+    return formatted;
+  };
 
-    return (
-        <div className={`cardform ${isValid ? 'is-valid' : ''}`}>
-            <h1>Credit Card Validation</h1>
+  const handleCardInputChange = (e) => {
+    e.target.value = formatCardNumber(e.target.value);
+    setCardNumber(e.target.value);
 
-            <input
-                type="text"
-                value={cardNumber}
-                onChange={(e) => setCardNumber(e.target.value)}
-                placeholder="Enter your card number"
-                className="card-input"
-            />
+    // Clear the validation message if the input is empty
+    if (!e.target.value || e.target.value.trim() === "") {
+      setIsValid(null);
+    }
+  };
 
-            <button onClick={validateCard} className="validate-button">
-                {loading ? "Validating..." : "Validate"}
-            </button>
+  const validateCard = async () => {
+    if (!cardNumber || cardNumber.trim() === "") {
+      return;
+    }
 
-            {isValid !== null && (
-                <p>{isValid ? "Valid Card Number!" : "Invalid Card Number."}</p>
-            )}
-        </div>
-    );
-}
+    setLoading(true);
+
+    console.log("Validating card:", cardNumber);
+
+    try {
+      const response = await axios.post(
+        "https://card-validation-8778dda2604a.herokuapp.com/validate",
+        { cardNumber: cardNumber.replace(/\s+/g, "") } // removing spaces before sending to server
+      );
+      setIsValid(response.data.isValid);
+    } catch (error) {
+      console.error("Error validating card:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <div className="headline">
+        <strong>Credit Card Information</strong>
+      </div>
+    <div className="container">
+      <div className={`cardform ${isValid ? "is-valid" : ""}`}>
+      </div>
+        <input
+          type="tel"
+          value={cardNumber}
+          maxLength="22"
+          minLength="12"
+          onChange={handleCardInputChange}
+          placeholder="Enter your card number"
+          className="card-input"
+        />
+        {isValid !== null && (
+          <div className="">{isValid ? "Valid Card Number!" : "Invalid Card Number."}</div>
+        )}
+        <button onClick={validateCard} className="validate-button">
+          {loading ? "Validating..." : "Validate"}
+        </button>
+    </div>
+    </>
+  );
+};
 
 export default CreditCardForm;
