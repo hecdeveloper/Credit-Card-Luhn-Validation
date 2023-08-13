31a42b93fc57f6ed083c47d0d41ac272c9ce31bf
Hector Ibarra <hibarra@imaginuity.com>
2023-08-13 16:25:48 -0500

    update styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import CreditCardForm from "./Components/CreditCardForm";
 
 function App() {
-  return(
-    <div>
-         <div className="headline">
+  return (
+    <>
+      <div className="headline">
         <strong>Credit Card Details</strong>
       </div>
+      <div className="fix">
       <CreditCardForm />
       </div>
-  )
+    </>
+  );
 }
 
 export default App;
