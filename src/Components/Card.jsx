c12bc7e84072886ca602cdf1c20cf3f034871a6b
Hector Ibarra <hibarra@imaginuity.com>
2023-08-13 20:40:15 -0500

    card type validations

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,15 +4,18 @@ import cardChip from './Images/chip.png';
 import mLogo from './Images/mastercard.png';
 import vLogo from './Images/visa.png';
 
-const Card = ({ cardNumber, cardName, expiration,ccv, cardType }) => {
+const Card = ({ cardNumber, cardName, expiration,ccv, cardType, isEmpty }) => {
   return (
     <div className="credit-card">
       <div className="images-row">
 
     <img className="card-chip" src={cardChip} alt="Card Logo" />
-    {cardType === "Mastercard" && <img className="card-logo" src={mLogo} alt="Mastercard Logo" />}
-        {cardType === "Visa" && <img className="card-logo" src={vLogo} alt="Visa Logo" />}
-        {cardType !== "Mastercard" && cardType !== "Visa" && <div className="unknown-type">Card Type</div>}
+    {cardType === "Mastercard" && !isEmpty  && <img className="card-logo" src={mLogo} alt="Mastercard Logo" />}
+        {cardType === "Visa" && !isEmpty &&  <img className="card-logo" src={vLogo} alt="Visa Logo" />}
+        { (isEmpty || (cardType !== "Mastercard" && cardType !== "Visa")) && 
+  <div className="unknown-type">Unknown</div> 
+}
+
       </div>
       <div className="card-number">{cardNumber || "•••• •••• •••• •••"}</div>
       <div className="card-row">
